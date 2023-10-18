7d39f8ec0a9a5fb596248b04381293514c5bf765
aaronpim <44268127+aaronpim@users.noreply.github.com>
2023-10-18 18:21:27 +0100

    Add files via upload

--- Forward_test.m
+++ Forward_test.m
@@ -1,3 +1,3 @@
 [model] = domain_defn(1);
 f = @(location, state) (location.x<0.4).*(location.x>0.2).*(location.y>-0.3).*(location.y<-0.1);
-forward_kolmogorov(model,f,0.1);
+[result] = forward_kolmogorov(model,f,0.1);
--- forward_kolmogorov.m
+++ forward_kolmogorov.m
@@ -1,7 +1,9 @@
 function [result] = forward_kolmogorov(model,f,epsilon)
 applyBoundaryCondition(model,"dirichlet","edge",[1,12],"u",0);
-ccoeff = @(location, state) [0 -((location.y).^2)/2 ((location.y).^2)/2 epsilon];
-specifyCoefficients(model,"m",0,"d",0,"c",ccoeff,"a",0,"f",f);
+syms u(x,y)
+pdeeq = y.*diff(u,x) -epsilon*diff(u,y,y);
+coeffs = pdeCoefficients(pdeeq,u);
+specifyCoefficients(model,"m",0,"d",0,"c",coeffs.c,"a",0,"f",f);
 generateMesh(model);
 result = solvepde(model); 
 end
